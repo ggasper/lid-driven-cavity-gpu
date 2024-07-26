be50e7f6600a4c5f13af952e2a3079b3cbf36a97
ggasper <gg.gasper@gmail.com>
2024-07-26 12:57:14 +0000

    Initilized a structure for storage on the GPU for sycl.

--- src/sycl/lidDrivenACMSYCL.cpp
+++ src/sycl/lidDrivenACMSYCL.cpp
@@ -4,9 +4,54 @@
 #include <Eigen/SparseLU>
 #include <chrono>
 #include <sycl/sycl.hpp>
+#include <numeric>
+#include <vector>
 #define EPS 1e-10
 
 using namespace mm;
+template <typename vec_t,
+          typename OpFamilies = std::tuple<Lap<vec_t::dim>, Der1s<vec_t::dim>, Der2s<vec_t::dim>>>
+class RaggedShapeStorageDevice {
+    typedef vec_t vector_t;
+    typedef typename vec_t::scalar_t scalar_t;
+    enum { dim = vec_t::dim };
+    typedef OpFamilies op_families_tuple;
+    constexpr static int num_operators = std::tuple_size<op_families_tuple>::value;
+    sycl::queue* q_;
+    int domain_size_;
+    int* support_;
+    std::vector<scalar_t*> shapes_;
+
+  public:
+    RaggedShapeStorageDevice(RaggedShapeStorage<vec_t, OpFamilies> storage, sycl::queue& q) {
+        q_ = &q;
+        auto support_sizes = storage.supportSizes();
+        int size = std::reduce(support_sizes.begin(), support_sizes.end());
+        support_ = sycl::malloc_device<int>(size, *q_);
+        int j = 0;
+        for (int i = 0; i < storage.size(); ++i) {
+            auto vec = storage.support(i);
+            q.submit([&](sycl::handler& h) {
+                h.memcpy(&support_[j], &vec.begin()[0], vec.size() * sizeof(int));
+            });
+            j += vec.size();
+        }
+        for (int i = 0; i < storage.shapes().size(); ++i) {
+            auto& shape = storage.shapes()[i];
+            shapes_.emplace_back(sycl::malloc_device<scalar_t>(shape.size(), *q_));
+            q.submit([&](sycl::handler& h) {
+                h.memcpy(shapes_[i], &shape[0], shape.size() * sizeof(scalar_t));
+            });
+        }
+        q.wait();
+    }
+    ~RaggedShapeStorageDevice() {
+        sycl::free(support_, *q_);
+        for (int i = 0; i < num_operators; ++i) {
+            sycl::free(shapes_[i], *q_);
+        }
+    }
+};
 
 template <int dim>
 class LidDrivenACM {
@@ -14,12 +59,10 @@ class LidDrivenACM {
     typedef double scal_t;
     typedef Vec<scal_t, dim> vec_t;
 
-    explicit LidDrivenACM(XML &param_file) {
-        HDF::Mode mode = param_file.get<bool>("output.clear_hdf5")
-                         ? HDF::DESTROY
-                         : HDF::APPEND;
+    explicit LidDrivenACM(XML& param_file) {
+        HDF::Mode mode = param_file.get<bool>("output.clear_hdf5") ? HDF::DESTROY : HDF::APPEND;
         HDF hdf_out(param_file.get<std::string>("output.hdf5_name"), mode);
-        
+
         auto Re = param_file.get<scal_t>("case.Re");
         auto h = param_file.get<scal_t>("num.h");
         auto cfl = param_file.get<scal_t>("num.cfl");
@@ -36,7 +79,7 @@ class LidDrivenACM {
         fill(domain, h);
 
         auto borders = box.bbox();
-        Range<int> corner = domain.positions().filter([&](const vec_t &p) {
+        Range<int> corner = domain.positions().filter([&](const vec_t& p) {
             // remove nodes that are EPS close to more than 1 border
             return (((p - borders.first) < EPS).size() + ((borders.second - p) < EPS).size()) > 1;
         });
@@ -51,14 +94,15 @@ class LidDrivenACM {
         Range<int> boundary = domain.boundary();
         Range<int> lid, wall;
         for (int i : boundary) {
-          if (borders.second[dim - 1] - domain.pos(i, dim - 1) < EPS) lid.push_back(i);
-          else wall.push_back(i);
+            if (borders.second[dim - 1] - domain.pos(i, dim - 1) < EPS)
+                lid.push_back(i);
+            else
+                wall.push_back(i);
         }
 
         scal_t midpoint = (borders.second(1) - borders.first(1)) / 2;
-        Range<int> midplane = domain.positions().filter([&](const vec_t &p) {
-            return std::abs(p(1) - midpoint) < h;
-        });
+        Range<int> midplane = domain.positions().filter(
+            [&](const vec_t& p) { return std::abs(p(1) - midpoint) < h; });
 
         std::cout << "h:" << h << ", N: " << N << ", dt:" << dt << std::endl;
 
@@ -77,18 +121,24 @@ class LidDrivenACM {
 
         auto k = param_file.get<int>("num.phs_order");
         Monomials<vec_t> mon(param_file.get<int>("num.mon_order"));
-        int support_size = std::round(param_file.get<scal_t>("num.support_size_factor") * mon.size());
+        int support_size =
+            std::round(param_file.get<scal_t>("num.support_size_factor") * mon.size());
 
         mm::RBFFD<Polyharmonic<scal_t>, vec_t, ScaleToClosest> rbffd(k, mon);
 
         domain.findSupport(FindClosest(support_size).forNodes(interior));
-        domain.findSupport(FindClosest(support_size).forNodes(boundary).searchAmong(interior).forceSelf(true));
+        domain.findSupport(
+            FindClosest(support_size).forNodes(boundary).searchAmong(interior).forceSelf(true));
 
-        std::cout << domain << std::endl;
         auto storage = domain.template computeShapes<sh::lap | sh::grad>(rbffd);
-        std::cout << "here?" << std::endl;
         auto op_e_v = storage.explicitVectorOperators();
         auto op_e_s = storage.explicitOperators();
+
+        // Move storage to GPU version
+        sycl::queue q(sycl::gpu_selector_v);
+        std::cout << "Selected device: " << q.get_device().get_info<sycl::info::device::name>()
+                  << "\n";
+        RaggedShapeStorageDevice storage_device{storage, q};
 
         scal_t t = 0;
         auto end_time = param_file.get<scal_t>("case.end_time");
@@ -97,39 +147,43 @@ class LidDrivenACM {
         auto div_limit = param_file.get<scal_t>("num.max_divergence");
         auto compress = param_file.get<scal_t>("acm.compressibility");
         auto v_ref = param_file.get<scal_t>("acm.reference_velocity");
-        int num_print = end_time/(dt * printout_interval);
+        int num_print = end_time / (dt * printout_interval);
         Eigen::VectorXd max_u_y(num_print);
         int iteration = 0;
         while (t < end_time) {
-            // #pragma omp parallel for default(none) schedule(static) shared(interior, u_partial, u, dt, op_e_v, Re)
+            // #pragma omp parallel for default(none) schedule(static) shared(interior, u_partial,
+            // u, dt, op_e_v, Re)
             for (int _ = 0; _ < interior.size(); ++_) {
                 int i = interior[_];
-                u_partial[i] = u[i] + dt * (op_e_v.lap(u, i) / Re
-                                         -  op_e_v.grad(u, i) * u[i]);
+                u_partial[i] = u[i] + dt * (op_e_v.lap(u, i) / Re - op_e_v.grad(u, i) * u[i]);
             }
             scal_t max_norm;
             scal_t max_div;
             int p_iter;
-            for(p_iter=0; p_iter < max_p_iter; ++p_iter) {
+            for (p_iter = 0; p_iter < max_p_iter; ++p_iter) {
                 std::swap(p_old, p_new);
                 max_norm = 0;
-                // #pragma omp parallel for default(none) schedule(static) shared(interior, u, u_partial, dt, op_e_s, p, p_old) reduction(max : max_norm)
+                // #pragma omp parallel for default(none) schedule(static) shared(interior, u,
+                // u_partial, dt, op_e_s, p, p_old) reduction(max : max_norm)
                 for (int _ = 0; _ < interior.size(); ++_) {
-                        int i = interior[_];
-                        u[i] = u_partial[i] - dt * op_e_s.grad(p[p_old], i);
-                        max_norm = std::max(u[i].norm(), max_norm);
+                    int i = interior[_];
+                    u[i] = u_partial[i] - dt * op_e_s.grad(p[p_old], i);
+                    max_norm = std::max(u[i].norm(), max_norm);
                 }
                 scal_t C = compress * std::max(max_norm, v_ref);
                 max_div = 0;
-                // #pragma omp parallel for default(none) schedule(static) shared(interior, op_e_v, u, p, p_new, p_old, C, dt, op_e_s, Re) reduction(max : max_div)
+                // #pragma omp parallel for default(none) schedule(static) shared(interior, op_e_v,
+                // u, p, p_new, p_old, C, dt, op_e_s, Re) reduction(max : max_div)
                 for (int _ = 0; _ < interior.size(); ++_) {
                     int i = interior[_];
                     scal_t div = op_e_v.div(u, i);
                     max_div = std::max(std::abs(div), max_div);
-                    p[p_new][i] = p[p_old][i] - C * C * dt * div;// + dt * op_e_s.lap(p[p_old], i) / Re;
+                    p[p_new][i] =
+                        p[p_old][i] - C * C * dt * div;  // + dt * op_e_s.lap(p[p_old], i) / Re;
                 }
 
-                // #pragma omp parallel for default(none) schedule(static) shared(boundary, p, p_new, op_e_s, domain)
+                // #pragma omp parallel for default(none) schedule(static) shared(boundary, p,
+                // p_new, op_e_s, domain)
                 for (int _ = 0; _ < boundary.size(); ++_) {
                     // Consider leaving one node constant to "fix" the pressure invariance
                     int i = boundary[_];
@@ -146,7 +200,7 @@ class LidDrivenACM {
                         pos = domain.pos(i, 0);
                     }
                 }
-                int print_iter = (iteration - 1)/printout_interval;
+                int print_iter = (iteration - 1) / printout_interval;
                 max_u_y[print_iter] = max;
                 std::cout << iteration << " - t:" << t << " max u_y:" << max << " @ x:" << pos
                           << "  (max div:" << max_div << " @ " << p_iter << ")" << std::endl;
@@ -168,33 +222,34 @@ class LidDrivenACM {
 
 // The path to .xml parameter file required as a command line parameter
 int main(int arg_num, char* arg[]) {
-    sycl::queue my_gpu_queue(sycl::gpu_selector_v);
-    std::cout << "Selected device: " << my_gpu_queue.get_device().get_info<sycl::info::device::name>() << "\n";
     if (arg_num < 2) {
-        throw std::invalid_argument("Missing command line argument. Provide path to .xml configuration.");
+        throw std::invalid_argument(
+            "Missing command line argument. Provide path to .xml configuration.");
     } else if (arg_num > 2) {
-        throw std::invalid_argument("Too many command line arguments. Only path to .xml configuration is required.");
+        throw std::invalid_argument(
+            "Too many command line arguments. Only path to .xml configuration is required.");
     }
     std::string parameter_file(arg[1]);
 
     XML params(parameter_file);
     if (params.get<bool>("output.use_xml_name")) {
-        std::string output_name = mm::join({mm::split(mm::split(parameter_file, "/").back(),".").front(), "h5"}, ".");
+        std::string output_name =
+            mm::join({mm::split(mm::split(parameter_file, "/").back(), ".").front(), "h5"}, ".");
         params.set("output.hdf5_name", output_name, true);
     }
     // omp_set_num_threads(params.get<int>("sys.num_threads"));
 
     switch (params.get<int>("case.dim")) {
-      case 2: {
-        LidDrivenACM<2> solution(params);
-        break;
-      }
-      case 3: {
-        LidDrivenACM<3> solution(params);
-        break;
-      }
-      default:
-        std::cout << params.get<int>("case.dim") << "is not a supported dimension" << std::endl; 
+        case 2: {
+            LidDrivenACM<2> solution(params);
+            break;
+        }
+        case 3: {
+            LidDrivenACM<3> solution(params);
+            break;
+        }
+        default:
+            std::cout << params.get<int>("case.dim") << "is not a supported dimension" << std::endl;
     }
 }
 
